a2548218013dd72f12be20216d9fe53f1f06d464
Ronaldrc <107775094+Ronaldrc@users.noreply.github.com>
2026-05-31 13:11:12 -0400

    Fix class name

--- src/test/java/bingodiscordlogger/BingoDiscordLoggerPluginTest.java
+++ src/test/java/bingodiscordlogger/BingoDiscordLoggerPluginTest.java
@@ -3,7 +3,7 @@ package bingodiscordlogger;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class RonHelmetPluginTest
+public class BingoDiscordLoggerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
